81bc6b6fa0e8219448ea335db16468ffa88ea1e2
SalvatoreGianquinto <gianquinto.salvatore11@gmail.com>
2025-09-03 12:53:58 +0200

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { Link } from "react-router-dom"
-import "../styles/NavBar.css"
+import "../styles/Navbar.css"
 
 const Navbar = () => {
   const [dropdownOpen, setDropdownOpen] = useState(false)
